00cc8c3012195275787d2388a0e063ee5c9cdf40
muzammiltnt10 <muzammiltnt10@users.noreply.github.com>
2018-04-29 23:24:37 +0530

    second test result success

--- app/src/main/java/com/example/zamzam/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zamzam/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.zamzam.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> candy_list = new ArrayList<String>();
+
+        candy_list.add("tropical wave");
+        candy_list.add("tropical wave");
+        candy_list.add("tropical wave");
+        candy_list.add("tropical wave");candy_list.add("tropical wave");
+        candy_list.add("tropical wave");
+        candy_list.add("tropical wave");
+        candy_list.add("tropical wave");
+        candy_list.add("tropical wave");
+        candy_list.add("tropical wave");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.text_view_candy,
+                R.id.text_view_candy,
+                candy_list);
+
+
+        ListView listView = (ListView)this.findViewById(R.id.list_view);
+
+        listView.setAdapter(adapter);
+
+
+
+
+
     }
 }
